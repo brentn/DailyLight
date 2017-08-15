17a8ac65082002596547860be7f6619aeeeed87b
brentn <nesbitt.brent@gmail.com>
2017-08-15 10:49:09 -0700

    Handle LORDS

--- angular/src/app/text.pipe.ts
+++ angular/src/app/text.pipe.ts
@@ -9,6 +9,7 @@ export class TextPipe implements PipeTransform {
   }
   private highlightLORD(value: string): string {
     return value.replace(/LORD'S/g, "<span class='lord'>LORD's</span>")
+      .replace(/LORDS/g, "L&zwnj;ORDS")
       .replace(/LORD/g, "<span class='lord'>LORD</span>")
   }
   private capitalizeFirstWord(value: string): string {
