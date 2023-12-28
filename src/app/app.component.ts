d421eb4c15f85af0f1848d272c7e88c57b5a2ae9
Brent Nesbitt <brent.nesbitt@p2c.com>
2023-12-28 09:57:56 -0800

    Enable swipe navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,8 @@ const EVENING_HOUR = 16;
   ]
 })
 export class AppComponent {
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
   title: string = 'Daily Light';
   version: 'KJV' | 'NIV' = 'KJV';
   cards: ICard[] = kjvVersion?.days;
@@ -67,5 +69,35 @@ export class AppComponent {
     this.version = (this.version === 'KJV') ? 'NIV' : 'KJV';
     this.cards = (this.version === 'KJV') ? kjvVersion?.days : nivVersion?.days;
     this.update();
+  }
+
+  isTouchDevice(): boolean {
+    return (('ontouchstart' in window) ||
+      (navigator.maxTouchPoints > 0));
+  }
+
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
+      const duration = time - this.swipeTime!;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30 // Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
+        if (swipe === 'next') {
+          this.nextDay();
+        } else {
+          this.priorDay();
+        }
+      }
+    }
   }
 }
